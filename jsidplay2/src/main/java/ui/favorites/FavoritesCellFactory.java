ff29667cbcc722c631ce4e331a3eb219b45f83c5
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2013-08-26 21:12:10 +0000

    small refactoring
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@316 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/favorites/FavoritesCellFactory.java
+++ jsidplay2/src/main/java/ui/favorites/FavoritesCellFactory.java
@@ -10,56 +10,61 @@ import javafx.util.Callback;
 import ui.JSIDPlay2Main;
 import ui.entities.collection.HVSCEntry;
 
-public class FavoritesCellFactory implements Callback<TableColumn<HVSCEntry, ?>, TableCell<HVSCEntry, ?>> {
+public class FavoritesCellFactory implements
+		Callback<TableColumn<HVSCEntry, ?>, TableCell<HVSCEntry, ?>> {
 
-	private static final Image STIL_ICON = new Image(JSIDPlay2Main.class.getResource("icons/stil.png").toString());
+	private static final Image STIL_ICON = new Image(JSIDPlay2Main.class
+			.getResource("icons/stil.png").toString());
 
-	private static final Image NO_STIL_ICON = new Image(JSIDPlay2Main.class.getResource("icons/stil_no.png").toString());
+	private static final Image NO_STIL_ICON = new Image(JSIDPlay2Main.class
+			.getResource("icons/stil_no.png").toString());
 
 	private static final String FILE_NOT_FOUND_ROW = "fileNotFoundRow";
 
 	private static final String CURRENTLY_PLAYED_FILE_ROW = "currentlyPlayedRow";
 
-	private FavoritesTab favoritesTab;
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public TableCell<HVSCEntry, ?> call(final TableColumn<HVSCEntry, ?> column) {
-		final TableCell<HVSCEntry, ?> cell = new TableCell() {
-
-			@Override
-			public void updateItem(Object value, boolean empty) {
-				super.updateItem(value, empty);
-				if (!empty) {
-					setText(value.toString());
-					if (getTableRow() != null) {
-						HVSCEntry hvscEntry = (HVSCEntry) getTableRow().getItem();
-						if (hvscEntry != null && favoritesTab != null) {
-							File file = favoritesTab.getFile(hvscEntry.getPath());
-							getStyleClass().remove(CURRENTLY_PLAYED_FILE_ROW);
-							getStyleClass().remove(FILE_NOT_FOUND_ROW);
-							if (file == null || !file.exists()) {
-								getStyleClass().add(FILE_NOT_FOUND_ROW);
+	@SuppressWarnings("rawtypes")
+	private final class TableCellImpl extends TableCell {
+		@SuppressWarnings("unchecked")
+		@Override
+		public void updateItem(Object value, boolean empty) {
+			super.updateItem(value, empty);
+			if (!empty) {
+				setText(value.toString());
+				if (getTableRow() != null) {
+					HVSCEntry hvscEntry = (HVSCEntry) getTableRow().getItem();
+					if (hvscEntry != null && favoritesTab != null) {
+						File file = favoritesTab.getFile(hvscEntry.getPath());
+						getStyleClass().remove(CURRENTLY_PLAYED_FILE_ROW);
+						getStyleClass().remove(FILE_NOT_FOUND_ROW);
+						if (file == null || !file.exists()) {
+							getStyleClass().add(FILE_NOT_FOUND_ROW);
+						} else if (file.equals(favoritesTab
+								.getCurrentlyPlayedFile())) {
+							getStyleClass().add(CURRENTLY_PLAYED_FILE_ROW);
+						}
+						int columnIndex = getTableView().getColumns().indexOf(
+								getTableColumn());
+						if (columnIndex == 0) {
+							if (favoritesTab.getStilEntry(hvscEntry.getPath()) != null) {
+								setGraphic(new ImageView(STIL_ICON));
 							} else {
-								if (file.equals(favoritesTab.getCurrentlyPlayedFile())) {
-									getStyleClass().add(CURRENTLY_PLAYED_FILE_ROW);
-								}
-							}
-							int columnIndex = column.getTableView().getColumns().indexOf(column);
-							if (columnIndex == 0) {
-								if (favoritesTab.getStilEntry(hvscEntry.getPath()) != null) {
-									setGraphic(new ImageView(STIL_ICON));
-								} else {
-									setGraphic(new ImageView(NO_STIL_ICON));
-								}
+								setGraphic(new ImageView(NO_STIL_ICON));
 							}
 						}
 					}
 				}
 			}
-		};
-		return cell;
+		}
 	}
+
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public TableCell<HVSCEntry, ?> call(final TableColumn<HVSCEntry, ?> arg0) {
+		return new TableCellImpl();
+	}
+
+	private FavoritesTab favoritesTab;
 
 	public void setFavoritesTab(FavoritesTab favoritesTab) {
 		this.favoritesTab = favoritesTab;
