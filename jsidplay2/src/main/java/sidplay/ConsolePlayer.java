fc017e6cccccce83333f69c9742ec01a79b113b1
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2014-05-09 18:33:05 +0000

    fine tuning options
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@483 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/sidplay/ConsolePlayer.java
+++ jsidplay2/src/main/java/sidplay/ConsolePlayer.java
@@ -27,10 +27,14 @@ import sidplay.ini.IniConfig;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 public class ConsolePlayer {
 	@Parameter(names = { "--help", "-h" }, help = true)
 	private Boolean help = Boolean.FALSE;
+
+	@Parameter(names = "--cpuDebug", hidden = true, description = "Display cpu register and assembly dumps")
+	private Boolean cpuDebug = Boolean.FALSE;
 
 	@Parameter(names = "-driver", description = "Audio driver (OUT_NULL, OUT_SOUNDCARD, OUT_WAV, OUT_MP3, OUT_LIVE_WAV or OUT_LIVE_MP3)", converter = OutputConverter.class)
 	private Output driver = Output.OUT_SOUNDCARD;
@@ -41,59 +45,64 @@ public class ConsolePlayer {
 	@Parameter(names = "-output", description = "Output filename for recording")
 	private String output = "outfile.wav";
 
-	@Parameter(names = "-o", description = "start track (default: tune start song)")
-	private Integer songNum = 0;
+	@Parameter(names = "-song", description = "start track (default: tune start song)")
+	private Integer song = 0;
 
-	@Parameter(names = "-l", description = "Loop track")
+	@Parameter(names = "-loop", description = "Loop track")
 	private Boolean loop = Boolean.FALSE;
 
-	@Parameter(names = "-s", description = "Single track")
+	@Parameter(names = "-single", description = "Single track")
 	private Boolean single = Boolean.FALSE;
 
-	@Parameter(names = "-f", description = "set frequency in Hz")
+	@Parameter(names = "-frequency", description = "set frequency in Hz")
 	private Integer frequency = 48000;
 
-	@Parameter(names = "-fd", description = "Force dual sid environment")
-	private Boolean forceStereoTune = Boolean.FALSE;
+	@Parameter(names = "-dualSID", description = "Force dual sid environment")
+	private Boolean dualSID = Boolean.FALSE;
 
-	@Parameter(names = "-vf", description = "Force user specific VIC PAL/NTSC clock speed (default: defined by song)", converter = CPUClockConverter.class)
-	private CPUClock userClock = null;
+	@Parameter(names = "-forceClock", description = "Force user specific VIC clock speed PAL or NTSC (default: defined by song)", converter = CPUClockConverter.class)
+	private CPUClock forceClock = null;
 
-	@Parameter(names = "-vd", description = "Set default VIC PAL/NTSC clock speed (to be used, if UNKNOWN)", converter = CPUClockConverter.class)
+	@Parameter(names = "-defaultClock", description = "Set default VIC clock speed PAL or NTSC (to be used, if UNKNOWN)", converter = CPUClockConverter.class)
 	private CPUClock defaultClock = CPUClock.PAL;
 
-	@Parameter(names = "-nf", description = "No SID filter emulation")
-	private Boolean disableFilters = Boolean.FALSE;
+	@Parameter(names = "-disableFilter", description = "No SID filter emulation")
+	private Boolean disableFilter = Boolean.FALSE;
 
-	@Parameter(names = "-nu", description = "Use waveforms MOS8580 or MOS6581 (default: from tune or cfg)", converter = ChipModelConverter.class)
-	private ChipModel userModel = null;
+	@Parameter(names = "-forceModel", description = "Use waveforms MOS8580 or MOS6581 (default: from tune or cfg)", converter = ChipModelConverter.class)
+	private ChipModel forceModel = null;
 
-	@Parameter(names = "-nd", description = "Default waveforms MOS8580 or MOS6581 (to be used, if UNKNOWN)", converter = ChipModelConverter.class)
+	@Parameter(names = "-defaultModel", description = "Default waveforms MOS8580 or MOS6581 (to be used, if UNKNOWN)", converter = ChipModelConverter.class)
 	private ChipModel defaultModel = ChipModel.MOS6581;
 
-	@Parameter(names = "-b", description = "Start time in [m:]s format", converter = TimeConverter.class)
+	@Parameter(names = "-startTime", description = "Start time in [m:]s format", converter = TimeConverter.class)
 	private Integer startTime = 0;
 
-	@Parameter(names = "-t", description = "Set fixed play length in [m:]s format (0 is endless)", converter = TimeConverter.class)
-	private Integer userPlayLength = 0;
+	@Parameter(names = "-fixedLength", description = "Set fixed play length in [m:]s format (0 is endless)", converter = TimeConverter.class)
+	private Integer fixedLength = 0;
 
-	@Parameter(names = "-v", description = "Verbose (level=0,1,2)", validateWith = VerboseValidator.class)
-	private Integer verboseLevel = 0;
+	@Parameter(names = "-verbose", description = "Verbose (level=0,1,2)", validateWith = VerboseValidator.class)
+	private Integer verbose = 0;
 
-	@Parameter(names = "-q", description = "Quiet (no output)")
+	@Parameter(names = "-quiet", description = "Quiet (no output)")
 	private Boolean quiet = Boolean.FALSE;
-
-	@Parameter(names = "--cpu-debug", hidden = true, description = "Display cpu register and assembly dumps")
-	private Boolean debug = Boolean.FALSE;
 
 	@Parameter(description = "filename")
 	private List<String> filenames = new ArrayList<String>();
 
 	public ConsolePlayer(String[] args) throws IOException {
-		JCommander commander = new JCommander(this, args);
-		if (help || filenames.size() != 1) {
-			commander.usage();
-			exit(help ? 0 : 1);
+		JCommander commander;
+		try {
+			commander = new JCommander(this, args);
+			commander.setProgramName(getClass().getName());
+			commander.setCaseSensitiveOptions(true);
+			if (help || filenames.size() != 1) {
+				commander.usage();
+				exit(help ? 0 : 1);
+			}
+		} catch (ParameterException e) {
+			System.err.println(e.getMessage());
+			exit(1);
 		}
 		// Can only loop if not creating audio files
 		if (isRecording()) {
@@ -103,14 +112,14 @@ public class ConsolePlayer {
 		final IniConfig config = new IniConfig(true);
 		config.getSidplay2().setLoop(loop);
 		config.getSidplay2().setSingle(single);
-		config.getSidplay2().setUserPlayLength(userPlayLength);
+		config.getSidplay2().setUserPlayLength(fixedLength);
 		config.getAudio().setFrequency(frequency);
-		config.getEmulation().setForceStereoTune(forceStereoTune);
-		config.getEmulation().setUserClockSpeed(userClock);
+		config.getEmulation().setForceStereoTune(dualSID);
+		config.getEmulation().setUserClockSpeed(forceClock);
 		config.getEmulation().setDefaultClockSpeed(defaultClock);
-		config.getEmulation().setUserSidModel(userModel);
+		config.getEmulation().setUserSidModel(forceModel);
 		config.getEmulation().setDefaultSidModel(defaultModel);
-		config.getEmulation().setFilter(!disableFilters);
+		config.getEmulation().setFilter(!disableFilter);
 
 		final Player player = new Player(config);
 		try {
@@ -119,11 +128,11 @@ public class ConsolePlayer {
 			e.printStackTrace();
 			exit(1);
 		}
-		player.setDebug(debug ? CPUParser.getInstance() : null);
+		player.setDebug(cpuDebug ? CPUParser.getInstance() : null);
 		player.setDriverSettings(new DriverSettings(driver, emulation));
 		player.getTune().setOutputFilename(output);
 		// Select the desired track and also mark the play-list start
-		player.getTrack().setSelected(player.getTune().selectSong(songNum));
+		player.getTrack().setSelected(player.getTune().selectSong(song));
 		player.getTrack().setFirst(0);
 		player.getTrack().setSongs(
 				config.getSidplay2().isSingle() ? 1 : player.getTune()
@@ -150,7 +159,7 @@ public class ConsolePlayer {
 				exit(1);
 			}
 		}
-		ConsoleIO consoleIO = new ConsoleIO(config, player, quiet, verboseLevel);
+		ConsoleIO consoleIO = new ConsoleIO(config, player, quiet, verbose);
 		player.setMenuHook((obj) -> consoleIO.menu(player.getTune(),
 				player.getTrack(), player.getTimer()));
 		player.setInteractivityHook((obj) -> consoleIO.decodeKeys());
