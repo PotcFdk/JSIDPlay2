85c2adcbbea1c18e562dd9bc32c747022d28eb83
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2014-06-16 20:15:54 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@599 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/siddump/SidDump.java
+++ jsidplay2/src/main/java/ui/siddump/SidDump.java
@@ -74,7 +74,7 @@ public class SidDump extends Tab implements UIPart {
 		if (newValue == State.RUNNING) {
 			Platform.runLater(() -> setTune(util.getPlayer().getTune()));
 		}
-		if (newValue == State.EXIT || newValue == State.QUIT) {
+		if (newValue == State.EXIT) {
 			Platform.runLater(() -> {
 				startStopRecording.setSelected(false);
 				replayAll.setDisable(false);
