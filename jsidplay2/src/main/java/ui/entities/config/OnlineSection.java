ffc176599a0026d3cc1346a3804015052a7ed206
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2015-02-17 08:08:06 +0000

    deployment URL repaired
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@952 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/entities/config/OnlineSection.java
+++ jsidplay2/src/main/java/ui/entities/config/OnlineSection.java
@@ -5,6 +5,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Optional;
 
 import javax.persistence.Embeddable;
 
@@ -13,25 +14,31 @@ import sidplay.ini.intf.IOnlineSection;
 @Embeddable
 public class OnlineSection implements IOnlineSection {
 
+	private static final String MAFIFEST_URL_KEY = "url:";
+
 	/**
 	 * URL where the JSIDPlay2 is deployed to.
 	 */
-	private static String DEPLOYMENT_URL;
+	private static final String DEPLOYMENT_URL;
 	static {
+		String codebase = "http://kenchis.t15.org/jsidplay2/javafx/";
 		try {
 			// Determine download URL from project URL
 			URI uri = OnlineSection.class.getResource("/META-INF/MANIFEST.MF")
 					.toURI();
 			FileSystems.newFileSystem(uri,
 					Collections.singletonMap("create", "true"));
-			Files.lines(Paths.get(uri))
-					.filter(s -> s.startsWith("url: "))
-					.forEach(
-							s -> DEPLOYMENT_URL = s.substring("url: ".length()));
+			Optional<String> url = Files.lines(Paths.get(uri))
+					.filter(s -> s.startsWith(MAFIFEST_URL_KEY)).findFirst();
+			if (url.isPresent()) {
+				codebase = url.get().substring(MAFIFEST_URL_KEY.length())
+						.trim();
+			}
 		} catch (Exception e) {
 			// MANIFEST.MF is only available in a release version!
-			DEPLOYMENT_URL = "http://kenchis.t15.org/jsidplay2/javafx/";
+			e.printStackTrace(System.err);
 		}
+		DEPLOYMENT_URL = codebase;
 	}
 
 	private String hvscUrl = DEPLOYMENT_URL + "C64Music.zip";
@@ -102,7 +109,8 @@ public class OnlineSection implements IOnlineSection {
 
 	private static final String SOASC_BASE = "http://www.se2a1.net/soasc/dl.php?d=";
 
-	private String soasc6581R2 = SOASC_BASE + "soasc/soasc_mp3{0}_T{1,number,00}.sid_MOS6581R2.mp3";
+	private String soasc6581R2 = SOASC_BASE
+			+ "soasc/soasc_mp3{0}_T{1,number,00}.sid_MOS6581R2.mp3";
 
 	@Override
 	public String getSoasc6581R2() {
@@ -114,7 +122,8 @@ public class OnlineSection implements IOnlineSection {
 		soasc6581R2 = soasc6581r2;
 	}
 
-	private String soasc6581R4 = SOASC_BASE + "soasc/soasc_mp3{0}_T{1,number,00}.sid_MOS6581R4.mp3";
+	private String soasc6581R4 = SOASC_BASE
+			+ "soasc/soasc_mp3{0}_T{1,number,00}.sid_MOS6581R4.mp3";
 
 	@Override
 	public String getSoasc6581R4() {
@@ -126,7 +135,8 @@ public class OnlineSection implements IOnlineSection {
 		soasc6581R4 = soasc6581r4;
 	}
 
-	private String soasc8580R5 = SOASC_BASE + "soasc/soasc_mp3{0}_T{1,number,00}.sid_CSG8580R5.mp3";
+	private String soasc8580R5 = SOASC_BASE
+			+ "soasc/soasc_mp3{0}_T{1,number,00}.sid_CSG8580R5.mp3";
 
 	@Override
 	public String getSoasc8580R5() {
