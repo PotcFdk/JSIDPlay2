58030fa44208878469f60dd2b5f29473d036cfd3
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2015-04-16 21:24:59 +0000

    update to current version and buffer size
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1035 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2app/src/de/haendel/jsidplay2/config/IConfiguration.java
+++ jsidplay2app/src/de/haendel/jsidplay2/config/IConfiguration.java
@@ -36,7 +36,7 @@ public interface IConfiguration {
 	String _48000 = "48000";
 	String _96000 = "96000";
 
-	String DEFAULT_BUFFER_SIZE = "2500";
+	String DEFAULT_BUFFER_SIZE = "25000";
 	String DEFAULT_PLAY_LENGTH = "300";
 	String DEFAULT_ENABLE_DATABASE = Boolean.TRUE.toString();
 	String DEFAULT_SINGLE_SONG = Boolean.FALSE.toString();
