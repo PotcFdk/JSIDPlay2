08fabc9d0ae8516df0f59e9dc4bf53f4eb99ec40
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2016-01-11 22:30:39 +0000

    do not stop or fade-in/-out mp3 tunes
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1281 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/sidplay/Player.java
+++ jsidplay2/src/main/java/sidplay/Player.java
@@ -182,12 +182,12 @@ public class Player extends HardwareEnsemble {
 			 * <LI>Single tune or end of play list? Stop player (except loop)
 			 * <LI>Else play next song
 			 * </UL>
-			 * 
+			 * <B>Note:</B> MP3 tunes are not stopped!
 			 * @see sidplay.player.Timer#end()
 			 */
 			@Override
 			public void end() {
-				if (tune != SidTune.RESET) {
+				if (tune != SidTune.RESET && !(tune instanceof MP3Tune)) {
 					if (config.getSidplay2Section().isSingle()
 							|| !playList.hasNext()) {
 						stateProperty.set(getEndState());
@@ -198,25 +198,27 @@ public class Player extends HardwareEnsemble {
 			}
 
 			/**
-			 * If a tune starts playing, fade-in volume.
+			 * If a tune starts playing, fade-in volume.<BR>
+			 * <B>Note:</B> MP3 tunes do not fade-in!
 			 * 
 			 * @see sidplay.player.Timer#fadeInStart(int)
 			 */
 			@Override
 			public void fadeInStart(int fadeIn) {
-				if (tune != SidTune.RESET) {
+				if (tune != SidTune.RESET && !(tune instanceof MP3Tune)) {
 					configureMixer(mixer -> mixer.fadeIn(fadeIn));
 				}
 			}
 
 			/**
-			 * If a tune is short before stop time, fade-out volume.
+			 * If a tune is short before stop time, fade-out volume.<BR>
+			 * <B>Note:</B> MP3 tunes do not fade-out!
 			 * 
 			 * @see sidplay.player.Timer#fadeOutStart(int)
 			 */
 			@Override
 			public void fadeOutStart(int fadeOut) {
-				if (tune != SidTune.RESET) {
+				if (tune != SidTune.RESET && !(tune instanceof MP3Tune)) {
 					configureMixer(mixer -> mixer.fadeOut(fadeOut));
 				}
 			}
