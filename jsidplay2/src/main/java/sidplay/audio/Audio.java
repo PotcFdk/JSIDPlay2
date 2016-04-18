7d0313b1602eb47c884cacba8b206f2dca0f4444
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2016-04-18 17:42:09 +0000

    MP3 moved to Audio (out-of Player)
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1392 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/sidplay/audio/Audio.java
+++ jsidplay2/src/main/java/sidplay/audio/Audio.java
@@ -1,5 +1,10 @@
 package sidplay.audio;
 
+import java.io.File;
+
+import libsidplay.config.IAudioSection;
+import libsidplay.sidtune.MP3Tune;
+import libsidplay.sidtune.SidTune;
 import sidplay.audio.MP3Driver.MP3File;
 
 public enum Audio {
@@ -16,7 +21,7 @@ public enum Audio {
 	/** Java Sound API plus playback of MP3 recording. */
 	COMPARE_MP3(new CmpMP3File());
 
-	private final AudioDriver audioDriver;
+	private AudioDriver oldAudioDriver;
 
 	private Audio(AudioDriver audioDriver) {
 		this.audioDriver = audioDriver;
@@ -25,4 +30,41 @@ public enum Audio {
 	public final AudioDriver getAudioDriver() {
 		return audioDriver;
 	}
+
+	private final AudioDriver audioDriver;
+
+	public final AudioDriver getAudioDriver(final IAudioSection audioSection, SidTune tune) {
+		return handleMP3(audioSection, tune, audioDriver);
+	}
+
+	/**
+	 * MP3 play-back is using the COMPARE audio driver. Old settings are saved
+	 * (playing mp3) and restored (next time normal tune is played).
+	 */
+	private AudioDriver handleMP3(final IAudioSection audioSection, final SidTune tune, final AudioDriver audioDriver) {
+		AudioDriver newAudioDriver = audioDriver;
+		if (oldAudioDriver == null && tune instanceof MP3Tune) {
+			// save settings before MP3 gets played
+			oldAudioDriver = audioDriver;
+		} else if (oldAudioDriver != null && !(tune instanceof MP3Tune)) {
+			// restore settings after MP3 has been played last time
+			newAudioDriver = oldAudioDriver;
+			oldAudioDriver = null;
+		}
+		if (tune instanceof MP3Tune) {
+			// Change driver settings to use comparison driver for MP3 play-back
+			MP3Tune mp3Tune = (MP3Tune) tune;
+			newAudioDriver = COMPARE_MP3.getAudioDriver();
+			audioSection.setPlayOriginal(true);
+			audioSection.setMp3File(mp3Tune.getMP3Filename());
+		}
+		if (newAudioDriver instanceof CmpMP3File) {
+			// Configure compare driver settings
+			CmpMP3File cmp = (CmpMP3File) newAudioDriver;
+			cmp.setPlayOriginal(audioSection.isPlayOriginal());
+			cmp.setMp3File(new File(audioSection.getMp3File()));
+		}
+		return newAudioDriver;
+	}
+
 }
