c531fa60eb3bd1d49c07a3d3279e710df5d35530
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2016-10-22 15:35:54 +0000

    just comments
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1555 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/hardsid_builder/HardSIDBuilder.java
+++ jsidplay2/src/main/java/hardsid_builder/HardSIDBuilder.java
@@ -27,10 +27,10 @@ import libsidplay.sidtune.SidTune;
  *  email                : s_a_white@email.com
  * **************************************************************************
  * **************************************************************************
- * * This program is free software; you can redistribute it and/or modify * it
- * under the terms of the GNU General Public License as published by * the Free
- * Software Foundation; either version 2 of the License, or * (at your option)
- * any later version. * *
+ * * This program is free software; you can redistribute it and/or modify 
+ * * it under the terms of the GNU General Public License as published by
+ * * the Free Software Foundation; either version 2 of the License, or
+ * * (at your option) any later version.
  * **************************************************************************
  * </pre>
  * 
