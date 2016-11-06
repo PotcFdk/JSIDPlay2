8a52195e96d92341ca82e6482bd70124dfe82fe1
bozz64 <bozz64@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2016-11-06 08:41:04 +0000

    Fix for Windows not displaying any files in the file open dialog. The previous case insensitive solution doesn't work on Windows. Now the list is extended with the Windows compatible way and the case insensitive way that works for other OSes.
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1600 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/filefilter/TuneFileExtensions.java
+++ jsidplay2/src/main/java/ui/filefilter/TuneFileExtensions.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface TuneFileExtensions {
-	final List<String> EXTENSIONS = Arrays.asList("*.[sS][iI][dD]", "*.[dD][aA][tT]", "*.[cC]64", "*.[pP][rR][gG]",
+	final List<String> EXTENSIONS = Arrays.asList("*.sid", "*.dat", "*.c64",
+			"*.prg", "*.p00", "*.t64", "*.mus", "*.str", "*.mp3", "*.zip",
+			"*.[sS][iI][dD]", "*.[dD][aA][tT]", "*.[cC]64", "*.[pP][rR][gG]",
 			"*.[pP]00", "*.[tT]64", "*.[mM][uU][sS]", "*.[sS][tT][rR]", "*.[mM][pP]3", "*.zip");
 
 	final String DESCRIPTION = "C64 Tunes";
