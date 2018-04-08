0debab86c671c8e6c3656ea001a6659979f62dd1
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2018-04-08 18:42:55 +0000

    NPE fixed
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@1961 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/webview/WebView.java
+++ jsidplay2/src/main/java/ui/webview/WebView.java
@@ -55,6 +55,9 @@ public class WebView extends Tab implements UIPart {
 		public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
 			if (State.SUCCEEDED.equals(newValue)) {
 				Document document = webView.getEngine().getDocument();
+				if (document == null) {
+					return;
+				}
 				NodeList anchors = document.getElementsByTagName(ANCHOR_TAG);
 				for (int i = 0; i < anchors.getLength(); i++) {
 					Node node = anchors.item(i);
