c2331a4bb93fdee8c1a7709a070c1177236d63b8
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2018-09-06 19:11:26 +0000

    version infos added to harsid methods
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2167 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/builder/sidblaster/HardSID.java
+++ jsidplay2/src/main/java/builder/sidblaster/HardSID.java
@@ -4,23 +4,19 @@ import com.sun.jna.Library;
 
 public interface HardSID extends Library {
 	enum HSID_USB_WSTATE {
-		HSID_USB_WSTATE_OK(1),
-		HSID_USB_WSTATE_BUSY(2),
-		HSID_USB_WSTATE_ERROR(3),
-		HSID_USB_WSTATE_END(4);
-		
+		HSID_USB_WSTATE_OK(1), HSID_USB_WSTATE_BUSY(2), HSID_USB_WSTATE_ERROR(3), HSID_USB_WSTATE_END(4);
+
 		private int rc;
 
 		private HSID_USB_WSTATE(int rc) {
 			this.rc = rc;
 		}
-		
+
 		public int getRc() {
 			return rc;
 		}
 	};
 
-	
 	// Version 2 Interface (Cycle exact interface)
 
 	void HardSID_Delay(byte DeviceID, short Cycles);
@@ -50,7 +46,7 @@ public interface HardSID extends Library {
 	byte HardSID_Write(byte DeviceID, short Cycles, byte SID_reg, byte data);
 
 	/**
-	 * Internal version number for this DLL e.g. 0x0202 (2.2)
+	 * Internal version number for this DLL e.g. 0x0202 (2.02)
 	 */
 	short HardSID_Version();
 
@@ -89,14 +85,21 @@ public interface HardSID extends Library {
 
 	/**
 	 * Click reduction
+	 * 
+	 * @since 2.04
 	 */
 	void HardSID_Reset2(byte DeviceID, byte volume);
 
 	/**
 	 * Lock SID to application
+	 * 
+	 * @since 2.04
 	 */
 	boolean HardSID_Lock(byte DeviceID);
 
+	/**
+	 * @since 2.04
+	 */
 	void HardSID_Unlock(byte DeviceID);
 
 	/**
@@ -104,6 +107,8 @@ public interface HardSID extends Library {
 	 * existing group. If deviceID = groupID then a new group is created with the
 	 * SID device becoming group master. Only writes to the master are played on the
 	 * other grouped SIDs.
+	 * 
+	 * @since 2.04
 	 */
 	boolean HardSID_Group(byte deviceID, boolean enable, byte groupID);
 
@@ -113,6 +118,8 @@ public interface HardSID extends Library {
 	 * Support whether the channel change was a request from the user or the program
 	 * (auto or manual respectively). External mixers can use this to prioritise
 	 * requests
+	 * 
+	 * @since 2.07
 	 */
 	void HardSID_Mute2(byte DeviceID, byte channel, boolean mute, boolean manual);
 
@@ -120,15 +127,26 @@ public interface HardSID extends Library {
 
 	/**
 	 * Enable support for non hardsid hardware (e.g. Catweasel MK3/4)
+	 * 
+	 * @since 2.08
 	 */
 	void HardSID_OtherHardware();
 
 	// Version 2.09 Extensions
 
+	/**
+	 * @since 2.09
+	 */
 	short HardSID_Clock(byte DeviceID, byte preset);
 
+	/**
+	 * @since 2.09
+	 */
 	void HardSID_SoftFlush(byte DeviceID);
 
+	/**
+	 * @since 2.09
+	 */
 	byte HardSID_Try_Write(byte DeviceID, short Cycles, byte SID_reg, byte data);
 
 }
