65d9c4b9dc1fd49010a0b11a0c42a55a9c83ef4f
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2018-09-30 07:27:16 +0000

    MP4 format corrected to play on Windows
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2202 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/sidplay/audio/MP4Driver.java
+++ jsidplay2/src/main/java/sidplay/audio/MP4Driver.java
@@ -12,24 +12,17 @@ import java.util.function.Consumer;
 
 import javax.sound.sampled.LineUnavailableException;
 
-import org.jcodec.api.transcode.AudioFrameWithPacket;
-import org.jcodec.api.transcode.PixelStore;
-import org.jcodec.api.transcode.PixelStore.LoanerPicture;
-import org.jcodec.api.transcode.PixelStoreImpl;
-import org.jcodec.api.transcode.Sink;
-import org.jcodec.api.transcode.SinkImpl;
-import org.jcodec.api.transcode.VideoFrameWithPacket;
-import org.jcodec.common.AudioFormat;
-import org.jcodec.common.Codec;
-import org.jcodec.common.Format;
-import org.jcodec.common.model.AudioBuffer;
+import org.jcodec.api.SequenceEncoder;
+import org.jcodec.common.io.NIOUtils;
 import org.jcodec.common.model.ColorSpace;
-import org.jcodec.common.model.Packet;
-import org.jcodec.common.model.Packet.FrameType;
 import org.jcodec.common.model.Picture;
 import org.jcodec.common.model.Rational;
-import org.jcodec.scale.ColorUtil;
-import org.jcodec.scale.Transform;
+import org.mp4parser.Container;
+import org.mp4parser.muxer.FileDataSourceImpl;
+import org.mp4parser.muxer.Movie;
+import org.mp4parser.muxer.builder.DefaultMp4Builder;
+import org.mp4parser.muxer.container.mp4.MovieCreator;
+import org.mp4parser.muxer.tracks.AACTrackImpl;
 import org.sheinbergon.aac.encoder.AACAudioEncoder;
 import org.sheinbergon.aac.encoder.AACAudioOutput;
 import org.sheinbergon.aac.encoder.WAVAudioInput;
@@ -37,136 +30,88 @@ import org.sheinbergon.aac.encoder.util.AACEncodingProfile;
 
 import libsidplay.common.CPUClock;
 import libsidplay.components.mos656x.VIC;
+import libsidutils.PathUtils;
 
 public class MP4Driver implements AudioDriver, Consumer<int[]> {
 
-	public class SequenceEncoder {
-		private Transform transform;
-		private int frameNo;
-		private int timestamp;
-		private Rational fps;
-		private Sink sink;
-		private PixelStore pixelStore;
-
-		public SequenceEncoder(String destName, Rational fps, Format outputFormat, Codec outputVideoCodec,
-				Codec outputAudioCodec) throws IOException {
-			this.fps = fps;
-			this.sink = new SinkImpl(destName, outputFormat, outputVideoCodec, outputAudioCodec) {
-				@Override
-				protected ByteBuffer encodeAudio(AudioBuffer audioBuffer) {
-					AudioFormat format = audioBuffer.getFormat();
-					AACAudioEncoder aacEncoder = AACAudioEncoder.builder().channels(format.getChannels())
-							.sampleRate(format.getSampleRate()).profile(AACEncodingProfile.AAC_LC).build();
-					WAVAudioInput input = WAVAudioInput.pcms16le(audioBuffer.getData().array(),
-							audioBuffer.getData().capacity());
-					AACAudioOutput output = aacEncoder.encode(input);
-					ByteBuffer result = ByteBuffer.wrap(output.data(), 0, output.length());
-//	                ADTSParser.read(result);
-					return result;
-				}
-
-			};
-			this.sink.init();
-			this.transform = ColorUtil.getTransform(ColorSpace.RGB, sink.getInputColor());
-			this.pixelStore = new PixelStoreImpl();
-		}
-
-		public void encodeNativeFrame(Picture pic) throws IOException {
-			ColorSpace sinkColor = sink.getInputColor();
-			LoanerPicture toEncode;
-			toEncode = pixelStore.getPicture(pic.getWidth(), pic.getHeight(), sinkColor);
-			transform.transform(pic, toEncode.getPicture());
-
-			Packet pkt = Packet.createPacket(null, timestamp, fps.getNum(), fps.getDen(), frameNo, FrameType.KEY, null);
-			sink.outputVideoFrame(new VideoFrameWithPacket(pkt, toEncode));
-
-			pixelStore.putBack(toEncode);
-
-			timestamp += fps.getDen();
-			frameNo++;
-		}
-
-		public void encodeAudioFrame(AudioBuffer audio) throws IOException {
-			Packet pkt = Packet.createPacket(null, timestamp, fps.getNum(), fps.getDen(), frameNo, FrameType.KEY, null);
-			sink.outputAudioFrame(new AudioFrameWithPacket(audio, pkt));
-		}
-
-		public void finish() throws IOException {
-			sink.finish();
-		}
-	}
-
-	private SequenceEncoder encoder;
+	private AACAudioEncoder aacEncoder;
+	private SequenceEncoder sequenceEncoder;
 	private byte[][] pictureData;
-	private AudioFormat audioDataFormat;
 	private ByteBuffer sampleBuffer;
-	private OutputStream audioDataStream;
-	private File audioDataFile;
+	private OutputStream pcmAudioStream;
+	private File pcmAudioFile;
+	private String recordingFilename;
+	private File h264VideoFile;
+	private File aacAudioFile;
 
 	@Override
 	public void open(AudioConfig cfg, String recordingFilename, CPUClock cpuClock)
 			throws IOException, LineUnavailableException {
-		final Rational rational = Rational.R((int) cpuClock.getScreenRefresh(), 1);
 		System.out.println("Recording, file=" + recordingFilename);
-		this.encoder = new SequenceEncoder(recordingFilename, rational, Format.MOV, Codec.H264, Codec.AAC);
+
+		String recordingBaseName = PathUtils.getFilenameWithoutSuffix(recordingFilename);
+		this.h264VideoFile = new File(recordingBaseName + ".h264");
+		this.aacAudioFile = new File(recordingBaseName + ".aac");
+		this.pcmAudioFile = new File(recordingBaseName + ".pcm");
+		this.pcmAudioStream = new FileOutputStream(pcmAudioFile);
+		this.recordingFilename = recordingFilename;
+
+		this.aacEncoder = AACAudioEncoder.builder().channels(2).sampleRate(cfg.getFrameRate())
+				.profile(AACEncodingProfile.AAC_LC).build();
+		this.sequenceEncoder = SequenceEncoder.createWithFps(NIOUtils.writableChannel(h264VideoFile),
+				Rational.R((int) cpuClock.getScreenRefresh(), 1));
 
 		this.pictureData = new byte[1][3 * VIC.MAX_WIDTH * VIC.MAX_HEIGHT];
 
 		this.sampleBuffer = ByteBuffer.allocate(cfg.getChunkFrames() * Short.BYTES * cfg.getChannels())
 				.order(ByteOrder.LITTLE_ENDIAN);
-
-		this.audioDataFile = File.createTempFile("mp4audio", ".pcm", new File(recordingFilename).getParentFile());
-		this.audioDataFile.deleteOnExit();
-		this.audioDataStream = new FileOutputStream(audioDataFile);
-		this.audioDataFormat = getAudioFormat(cfg);
 	}
 
 	@Override
 	public void write() throws InterruptedException {
 		try {
-			audioDataStream.write(buffer().array());
+			this.pcmAudioStream.write(buffer().array());
 		} catch (IOException e) {
-			throw new RuntimeException("Error writing audio data stream");
+			throw new RuntimeException("Error writing PCM audio stream", e);
 		}
 	}
 
 	@Override
 	public void accept(int[] bgraData) {
 		try {
-			this.encoder.encodeNativeFrame(createPicture(bgraData));
+			this.sequenceEncoder.encodeNativeFrame(createPicture(bgraData));
 		} catch (IOException e) {
-			throw new RuntimeException("Error during encodeNativeFrame");
+			throw new RuntimeException("Error writing H264 video stream", e);
 		}
 	}
 
 	@Override
 	public void close() {
 		try {
-			if (encoder != null) {
-				if (audioDataStream != null) {
-					audioDataStream.close();
-					byte[] data = Files.readAllBytes(Paths.get(audioDataFile.getAbsolutePath()));
-					if (data != null && data.length > 0) {
-						encoder.encodeAudioFrame(new AudioBuffer(ByteBuffer.wrap(data), audioDataFormat, 0));
-					}
-					audioDataFile.delete();
+			if (sequenceEncoder != null) {
+				sequenceEncoder.finish();
+				sequenceEncoder = null;
+			}
+			if (pcmAudioStream != null) {
+				pcmAudioStream.close();
+				pcmAudioStream = null;
+				byte[] data = Files.readAllBytes(Paths.get(pcmAudioFile.getAbsolutePath()));
+				try (OutputStream out = new FileOutputStream(aacAudioFile)) {
+					AACAudioOutput output = aacEncoder.encode(WAVAudioInput.pcms16le(data, data.length));
+					out.write(output.data(), 0, output.length());
 				}
-				encoder.finish();
-				encoder = null;
+				pcmAudioFile.delete();
+				Movie movie = MovieCreator.build(h264VideoFile.getAbsolutePath());
+				movie.addTrack(new AACTrackImpl(new FileDataSourceImpl(aacAudioFile)));
+				Container mp4file = new DefaultMp4Builder().build(movie);
+				try (FileOutputStream out = new FileOutputStream(new File(recordingFilename))) {
+					mp4file.writeContainer(out.getChannel());
+				}
+				aacAudioFile.delete();
+				h264VideoFile.delete();
 			}
 		} catch (IOException e) {
-			throw new RuntimeException("Error finishing encoder");
-		}
-	}
-
-	private AudioFormat getAudioFormat(AudioConfig cfg) {
-		switch (cfg.getFrameRate()) {
-		case 44100:
-			return AudioFormat.STEREO_44K_S16_LE;
-		case 48000:
-			return AudioFormat.STEREO_48K_S16_LE;
-		default:
-			throw new RuntimeException("Unsupported sampling rate for video recording");
+			throw new RuntimeException("Error creating MP4", e);
 		}
 	}
 
