db674dd0013a43b4b69eae647fb14217b577ef83
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2018-12-01 22:38:21 +0000

    cmp with orig code
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2286 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/libsidutils/assembler/KickAssembler.java
+++ jsidplay2/src/main/java/libsidutils/assembler/KickAssembler.java
@@ -2,6 +2,7 @@ package libsidutils.assembler;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,7 @@ import kickass.common.exceptions.AsmErrorException;
 import kickass.parsing.sourcelocation.SourceRange;
 import kickass.pass.asmnode.AsmNode;
 import kickass.pass.asmnode.metanodes.AsmNodeList;
+import kickass.pass.asmnode.metanodes.AsmNodePair;
 import kickass.pass.asmnode.metanodes.NamespaceNode;
 import kickass.pass.asmnode.metanodes.ScopeAndSymbolPageNode;
 import kickass.pass.asmnode.output.reciever.MainOutputReciever;
@@ -22,65 +24,66 @@ import kickass.state.scope.symboltable.SymbolStatus;
 
 public class KickAssembler {
 
-	private EvaluationState evaluationState;
-
 	/**
 	 * @return assembly bytes of the ASM resource
 	 */
 	public KickAssemblerResult assemble(String resource, InputStream asm, final Map<String, String> globals) {
+		final EvaluationState var1 = new EvaluationState();
 		try {
-			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			evaluationState = new EvaluationState();
-			evaluationState.outputMgr = (var1, var2) -> os;
-			HashtableValue hashtableValue = new HashtableValue().addStringValues(globals);
-			hashtableValue.lock((SourceRange) null);
-			evaluationState.namespaceMgr.getSystemNamespace().getScope()
-					.defineErrorIfExist("cmdLineVars", arrReferenceValue -> new ConstantValueHolder(hashtableValue),
-							evaluationState, "ERROR! cmdLineVars is already defined", (SourceRange) null)
+			ByteArrayOutputStream result = new ByteArrayOutputStream();
+			var1.outputMgr = (a, b) -> result;
+			HashtableValue var5 = new HashtableValue().addStringValues(globals);
+			var5.lock((SourceRange) null);
+			var1.namespaceMgr.getSystemNamespace().getScope()
+					.defineErrorIfExist("cmdLineVars", var1x -> new ConstantValueHolder(var5), var1,
+							"ERROR! cmdLineVars is already defined", (SourceRange) null)
 					.setStatus(SymbolStatus.defined);
 
-			AsmNode asmNode = AssemblerToolbox.loadAndLexOrError(asm, resource, evaluationState, (SourceRange) null);
-			if (asmNode == null) {
+			var1.prepareNewPass();
+			AsmNode var15 = AssemblerToolbox.loadAndLexOrError(asm, resource, var1, (SourceRange) null);
+			if (var15 == null) {
 				throw new RuntimeException("Parse error for assembler resource: " + resource);
 			}
-			asmNode = new NamespaceNode(asmNode, evaluationState.namespaceMgr.getRootNamespace());
-			AsmNodeList asmNodeList = new AsmNodeList(asmNode);
-			ScopeAndSymbolPageNode scopeAndSymbolPageNode = new ScopeAndSymbolPageNode(asmNodeList,
-					evaluationState.namespaceMgr.getSystemNamespace().getScope());
-			evaluationState.prepareNewPass();
-			AsmNode asmNode2 = scopeAndSymbolPageNode.executeMetaRegistrations(evaluationState);
-			asmNode2 = asmNode2.executePrepass(evaluationState);
-			printErrorsAndTerminate(evaluationState);
+			NamespaceNode var51 = new NamespaceNode(var15, var1.namespaceMgr.getRootNamespace());
+			var15 = var51.executeMetaRegistrations(var1);
+			AsmNodeList var52 = new AsmNodeList(new ArrayList<>());
+			AsmNode var53 = var52.executeMetaRegistrations(var1);
+			AsmNodePair var54 = new AsmNodePair(var53, var15);
+			ScopeAndSymbolPageNode var55 = new ScopeAndSymbolPageNode(var54,
+					var1.namespaceMgr.getSystemNamespace().getScope());
+			AsmNode var56 = var55.executePrepass(var1);
+			printErrorsAndTerminate(var1);
 			do {
-				evaluationState.prepareNewPass();
-				asmNode2 = asmNode2.executePass(evaluationState);
-				evaluationState.segmentMgr.postPassExecution();
-				if (!evaluationState.getMadeMetaProgress() && !asmNode2.isFinished()) {
-					evaluationState.prepareNewPass();
-					evaluationState.setFailOnInvalidValue(true);
-					asmNode2 = asmNode2.executePass(evaluationState);
+				var1.prepareNewPass();
+				var56 = var56.executePass(var1);
+				var1.segmentMgr.postPassExecution();
+				printErrorsAndTerminate(var1);
+				if (!var1.getMadeMetaProgress() && !var56.isFinished()) {
+					var1.prepareNewPass();
+					var1.setFailOnInvalidValue(true);
+					var56.executePass(var1);
 					throw new AsmErrorException(
 							"Made no progress and can\'t solve the program. You should have gotten an error. Contact the author!",
 							(SourceRange) null);
 				}
-			} while (!asmNode2.isFinished());
-			MainOutputReciever mainOutputReciever = new MainOutputReciever(evaluationState.outputMgr,
-					evaluationState.log);
-			asmNode2.deliverOutput(mainOutputReciever);
-			mainOutputReciever.finish();
-			evaluationState.segmentMgr.postPassesExecution();
-			printErrorsAndTerminate(evaluationState);
-			evaluationState.c64OutputMgr.postPassExecution();
-			printErrorsAndTerminate(evaluationState);
-			evaluationState.segmentMgr.doOutputAfterPasses();
+			} while (!var56.isFinished());
+			MainOutputReciever var25 = new MainOutputReciever(var1.outputMgr, var1.log);
+			var56.deliverOutput(var25);
+			var25.finish();
+			var1.segmentMgr.postPassesExecution();
+			printErrorsAndTerminate(var1);
+			var1.c64OutputMgr.postPassExecution();
+			printErrorsAndTerminate(var1);
+			var1.segmentMgr.doOutputAfterPasses();
+			printErrorsAndTerminate(var1);
 
-			Map<String, Integer> resolvedSymbols = evaluationState.scopeMgr.getResolvedSymbols().stream()
+			Map<String, Integer> resolvedSymbols = var1.scopeMgr.getResolvedSymbols().stream()
 					.collect(Collectors.toMap(res -> res.name, res -> res.address, (entry1, entry2) -> entry2));
-			return new KickAssemblerResult(os.toByteArray(), resolvedSymbols);
+			return new KickAssemblerResult(result.toByteArray(), resolvedSymbols);
 		} catch (AsmErrorException e) {
 			AsmError asmError = e.getError();
-			asmError.setCallStack(evaluationState.callStack);
-			System.err.println(OneLineErrorPrinter.instance.printError(asmError, evaluationState));
+			asmError.setCallStack(var1.callStack);
+			System.err.println(OneLineErrorPrinter.instance.printError(asmError, var1));
 			throw new AsmErrorException(asmError);
 		} catch (Exception e) {
 			throw new RuntimeException("Internal Error!", e);
