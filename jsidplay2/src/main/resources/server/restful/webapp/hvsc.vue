ab815098412d589bdef2a0c7939edd9c30c10f4c
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2019-12-01 18:18:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2490 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/resources/server/restful/webapp/hvsc.vue
+++ jsidplay2/src/main/resources/server/restful/webapp/hvsc.vue
@@ -14,14 +14,21 @@
 
 	<div id="app">
 		<ul>
-			<li v-for="entry in directory">
+			<li v-for="entry in directory" :key="entry">
 				<div v-if="entry.endsWith('/')">
-					<a href="#" v-on:click="fetchData(entry.substring(0, entry.length-1))">
+					<a href="#"
+						v-on:click="fetchData('directory', entry.substring(0, entry.length-1))">
 						{{entry}} </a>
 				</div>
 				<div v-else>
-					<a v-bind:href="'/jsidplay2service/JSIDPlay2REST/convert' + entry + '?defaultLength=03:00&enableSidDatabase=true&single=true&loop=false&bufferSize=65536&sampling=RESAMPLE&frequency=MEDIUM&defaultEmulation=RESIDFP&defaultModel=MOS8580&filter6581=FilterAlankila6581R4AR_3789&stereoFilter6581=FilterAlankila6581R4AR_3789&thirdFilter6581=FilterAlankila6581R4AR_3789&filter8580=FilterAlankila6581R4AR_3789&stereoFilter8580=FilterAlankila6581R4AR_3789&thirdFilter8580=FilterAlankila6581R4AR_3789&reSIDfpFilter6581=FilterAlankila6581R4AR_3789&reSIDfpStereoFilter6581=FilterAlankila6581R4AR_3789&reSIDfpThirdFilter6581=FilterAlankila6581R4AR_3789&reSIDfpFilter8580=FilterAlankila6581R4AR_3789&reSIDfpStereoFilter8580=FilterAlankila6581R4AR_3789&reSIDfpThirdFilter8580=FilterAlankila6581R4AR_3789&digiBoosted8580=true&cbr=64&vbrQuality=0&vbr=true'">
-						{{entry}} </a>
+					<div>
+						<a
+							v-bind:href="'/jsidplay2service/JSIDPlay2REST/convert' + entry + '?defaultLength=03:00&enableSidDatabase=true&single=true&loop=false&bufferSize=65536&sampling=RESAMPLE&frequency=MEDIUM&defaultEmulation=RESIDFP&defaultModel=MOS8580&filter6581=FilterAlankila6581R4AR_3789&stereoFilter6581=FilterAlankila6581R4AR_3789&thirdFilter6581=FilterAlankila6581R4AR_3789&filter8580=FilterAlankila6581R4AR_3789&stereoFilter8580=FilterAlankila6581R4AR_3789&thirdFilter8580=FilterAlankila6581R4AR_3789&reSIDfpFilter6581=FilterAlankila6581R4AR_3789&reSIDfpStereoFilter6581=FilterAlankila6581R4AR_3789&reSIDfpThirdFilter6581=FilterAlankila6581R4AR_3789&reSIDfpFilter8580=FilterAlankila6581R4AR_3789&reSIDfpStereoFilter8580=FilterAlankila6581R4AR_3789&reSIDfpThirdFilter8580=FilterAlankila6581R4AR_3789&digiBoosted8580=true&cbr=64&vbrQuality=0&vbr=true'">
+							{{entry}} </a>
+					</div>
+					<div>
+						<img v-bind:src="'/jsidplay2service/JSIDPlay2REST/photo' + entry" />
+					</div>
 				</div>
 			</li>
 		</ul>
@@ -31,27 +38,32 @@
 	new Vue({
 		el: '#app',
 		data: {
-			directory: ''
+			directory: '',
+			imgData: []
 		},
 		created: function () {
-			this.fetchData('/');
+			this.fetchData('directory', '/');
 		},        
 		methods: {
-			fetchData: function (entry) {
-				axios({
-					method: 'get',
-					url: '/jsidplay2service/JSIDPlay2REST/directory' + entry,
-					withCredentials: true,
-					auth: {
-						username: 'jsidplay2',
-						password: 'jsidplay2!'
-					}
-				}).then(response => {
-					this.directory = response.data
-				})
+			fetchData: function (type, entry) {
+				if (type == 'directory') {
+					axios({
+						method: 'get',
+						url: '/jsidplay2service/JSIDPlay2REST/' + type + entry,
+						withCredentials: true,
+						auth: {
+							username: 'jsidplay2',
+							password: 'jsidplay2!'
+						}
+					}).then(response => {
+						this.directory= response.data;
+					})
+				}
 			}
 		}
 	})
 </script>
 </body>
 </html>
+
+
