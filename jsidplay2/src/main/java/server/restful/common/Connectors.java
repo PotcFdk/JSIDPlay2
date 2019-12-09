6982db8419b27e7b729af11af32b929b971ea407
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2019-12-09 18:52:54 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2509 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/restful/common/Connectors.java
+++ jsidplay2/src/main/java/server/restful/common/Connectors.java
@@ -4,11 +4,11 @@ public enum Connectors {
 	HTTP("http", 8080), HTTP_HTTPS("https", 8443), HTTPS("https", 8443);
 
 	private String preferredProtocol;
-	private int portNum;
+	private int preferredPortNum;
 
 	private Connectors(String preferredProtocol, int portNum) {
 		this.preferredProtocol = preferredProtocol;
-		this.portNum = portNum;
+		this.preferredPortNum = portNum;
 	}
 
 	public String getPreferredProtocol() {
@@ -16,6 +16,6 @@ public enum Connectors {
 	}
 	
 	public int getPreferredPortNum() {
-		return portNum;
+		return preferredPortNum;
 	}
 }
