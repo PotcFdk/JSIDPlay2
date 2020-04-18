a000954409b0edf3988776b20cdd80a91803d314
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2020-04-18 05:59:10 +0000

    whatssid.vue added for WAV upload and tune recognition
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2768 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/restful/common/JSIDPlay2Servlet.java
+++ jsidplay2/src/main/java/server/restful/common/JSIDPlay2Servlet.java
@@ -1,5 +1,9 @@
 package server.restful.common;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Base64;
+
 import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.HttpHeaders;
 import javax.xml.bind.JAXBContext;
 
+import org.apache.tomcat.util.http.fileupload.FileItemIterator;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import libsidutils.ZipFileUtils;
+import sidplay.fingerprinting.WavBean;
 
 @SuppressWarnings("serial")
 public abstract class JSIDPlay2Servlet extends HttpServlet {
@@ -24,6 +34,22 @@ public abstract class JSIDPlay2Servlet extends HttpServlet {
 			} else if (MimeType.MIME_TYPE_XML.getContentType().startsWith(request.getContentType())) {
 				// MimeType.MIME_XML
 				return (T) JAXBContext.newInstance(tClass).createUnmarshaller().unmarshal(inputStream);
+			} else if (tClass.equals(WavBean.class)) {
+				StringBuilder result = new StringBuilder();
+				result.append("{ \"wav\": \"");
+				ServletFileUpload fileUpload = new ServletFileUpload();
+				FileItemIterator items = fileUpload.getItemIterator(request);
+				while (items.hasNext()) {
+					try (InputStream itemInputStream = items.next().openStream()) {
+						ByteArrayOutputStream bos = new ByteArrayOutputStream();
+						ZipFileUtils.copy(itemInputStream, bos);
+						result.append(Base64.getEncoder().encodeToString(bos.toByteArray()));
+					}
+					// just the first file
+					break;
+				}
+				result.append("\"}");
+				return new ObjectMapper().readValue(result.toString(), tClass);
 			} else {
 				throw new RuntimeException("Unsupported content type: " + request.getContentType());
 			}
@@ -37,14 +63,19 @@ public abstract class JSIDPlay2Servlet extends HttpServlet {
 			return;
 		}
 		try (ServletOutputStream out = response.getOutputStream()) {
-			if (request.getHeader(HttpHeaders.ACCEPT) == null
-					|| MimeType.MIME_TYPE_JSON.getContentType().startsWith(request.getHeader(HttpHeaders.ACCEPT))) {
-				response.setContentType(MimeType.MIME_TYPE_JSON.getContentType());
-				new ObjectMapper().writeValue(out, result);
-			} else if (MimeType.MIME_TYPE_XML.getContentType().startsWith(request.getHeader(HttpHeaders.ACCEPT))) {
-				// MimeType.MIME_XML
-				response.setContentType(MimeType.MIME_TYPE_XML.getContentType());
-				JAXBContext.newInstance(tClass).createMarshaller().marshal(result, out);
+			String[] acceptedHeaders = request.getHeader(HttpHeaders.ACCEPT).split(",");
+			for (String acceptedHeader : acceptedHeaders) {
+				if (acceptedHeader == null
+						|| MimeType.MIME_TYPE_JSON.getContentType().startsWith(acceptedHeader)) {
+					response.setContentType(MimeType.MIME_TYPE_JSON.getContentType());
+					new ObjectMapper().writeValue(out, result);
+					break;
+				} else if (MimeType.MIME_TYPE_XML.getContentType().startsWith(acceptedHeader)) {
+					// MimeType.MIME_XML
+					response.setContentType(MimeType.MIME_TYPE_XML.getContentType());
+					JAXBContext.newInstance(tClass).createMarshaller().marshal(result, out);
+					break;
+				}
 			}
 			out.flush();
 		} catch (Exception e) {
---
+++ jsidplay2/src/main/resources/server/restful/webapp/whatssid.vue
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>WhatsSid</title>
+</head>
+<body>
+	<!--script src="https://unpkg.com/vue"></script-->
+	<script src="https://cdn.jsdelivr.net/npm/vue@2.6.0"></script>
+	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+	<h1>WhatsSid?</h1>
+	<span>Please use username='jsidplay2', password='jsidplay2!'</span>
+
+	<div id="app">
+		<h1>Upload a WAV File</h1>
+		<form enctype="multipart/form-data">
+			<input type="file" name="file"
+				v-on:change="fileChange($event.target.files)" />
+			<button type="button" v-on:click="upload()">Upload</button>
+		</form>
+		<div>
+			<p>{{ match }}</p>
+		</div>
+	</div>
+
+	<script>
+
+	new Vue({
+        el: '#app',
+        data() {
+            return {
+            	match: '',
+                files: new FormData()
+            }
+        },
+        methods: {
+            fileChange(fileList) {
+                this.files.append("file", fileList[0], fileList[0].name);
+            },
+            upload() {
+            	this.match = 'Please wait...';
+                axios({ method: "POST", "url": "/jsidplay2service/JSIDPlay2REST/whatssid", "data": this.files }).then(result => {
+                	if (result.data && result.headers['content-length']) {
+	                	this.match = result.data;
+	               	} else {
+	               		this.match = 'Sorry, no match!';
+	               	}
+                    this.files = new FormData();
+                }, error => {
+                    console.error(error);
+                });
+            }
+        }
+	})
+</script>
+</body>
+</html>
