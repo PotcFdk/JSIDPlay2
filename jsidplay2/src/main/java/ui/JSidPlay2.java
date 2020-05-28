beb0b113b9e7eb6afc26dfdd2b667188b9b9ba62
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2020-05-28 22:30:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2880 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/JSidPlay2.java
+++ jsidplay2/src/main/java/ui/JSidPlay2.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import libsidplay.components.c1541.ExtendImagePolicy;
 import libsidplay.components.c1541.IExtendImageListener;
 import libsidplay.sidtune.SidTune;
@@ -32,7 +31,6 @@ public class JSidPlay2 extends C64Window implements IExtendImageListener, Functi
 
 	public JSidPlay2(Stage primaryStage, Player player) {
 		super(primaryStage, player);
-		getStage().initStyle(StageStyle.UTILITY);
 	}
 
 	@FXML
--- jsidplay2/src/main/java/ui/common/C64Window.java
+++ jsidplay2/src/main/java/ui/common/C64Window.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import sidplay.Player;
 import ui.entities.config.Configuration;
 import ui.entities.config.service.ConfigService;
@@ -72,6 +73,7 @@ public abstract class C64Window implements UIPart, Initializable {
 		}
 		stage.setOnCloseRequest(event -> close());
 		stage.setScene(scene);
+		stage.initStyle(StageStyle.UTILITY);
 	}
 
 	public void open() {
