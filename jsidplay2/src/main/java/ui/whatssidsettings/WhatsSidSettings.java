0690bb1d2d42a3095e9d2904ab6c9276276692a3
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2020-06-01 12:23:55 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@2898 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/whatssidsettings/WhatsSidSettings.java
+++ jsidplay2/src/main/java/ui/whatssidsettings/WhatsSidSettings.java
@@ -1,5 +1,14 @@
 package ui.whatssidsettings;
 
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_CAPTURE_TIME;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_ENABLE;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_MATCH_RETRY_TIME;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_MATCH_START_TIME;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_MINIMUM_RELATIVE_CONFIDENCE;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_PASSWORD;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_URL;
+import static sidplay.ini.IniDefaults.DEFAULT_WHATSSID_USERNAME;
+
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -44,6 +53,20 @@ public class WhatsSidSettings extends C64Window {
 		Bindings.bindBidirectional(minimumRelativeConfidence.textProperty(),
 				whatsSidSection.minimumRelativeConfidenceProperty(), new NumberStringConverter());
 
+	}
+
+	@FXML
+	private void restoreDefaults() {
+		WhatsSidSection whatsSidSection = util.getConfig().getWhatsSidSection();
+
+		whatsSidSection.setEnable(DEFAULT_WHATSSID_ENABLE);
+		whatsSidSection.setUrl(DEFAULT_WHATSSID_URL);
+		whatsSidSection.setUsername(DEFAULT_WHATSSID_USERNAME);
+		whatsSidSection.setPassword(DEFAULT_WHATSSID_PASSWORD);
+		whatsSidSection.setCaptureTime(DEFAULT_WHATSSID_CAPTURE_TIME);
+		whatsSidSection.setMatchStartTime(DEFAULT_WHATSSID_MATCH_START_TIME);
+		whatsSidSection.setMatchRetryTime(DEFAULT_WHATSSID_MATCH_RETRY_TIME);
+		whatsSidSection.setMinimumRelativeConfidence(DEFAULT_WHATSSID_MINIMUM_RELATIVE_CONFIDENCE);
 	}
 
 }
