c811d20736802b127511b5be4647c0fa86ee3337
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2020-12-19 11:08:19 +0000

    configuration changed triggered
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3027 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/libsidplay/config/IConfig.java
+++ jsidplay2/src/main/java/libsidplay/config/IConfig.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IConfig {
 
 	/** Bump this each time you want to invalidate the configuration */
-	int REQUIRED_CONFIG_VERSION = 22;
+	int REQUIRED_CONFIG_VERSION = 23;
 
 	ISidPlay2Section getSidplay2Section();
 
