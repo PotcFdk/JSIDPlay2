27eebf47a458689dcc12630f52e49f0a29a3cc5d
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-01-15 06:28:28 +0000

    shell scripts added for startup on macOS, Linux
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3080 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/JSIDPlay2 (Java11).sh
+++ jsidplay2/src/main/linux/JSIDPlay2 (Java11).sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/Downloads/${project.artifactId}-${project.version}-java11-linux
-export LD_LIBRARY_PATH=~/Downloads/${project.artifactId}-${project.version}-java11-linux
-java --module-path ~/Downloads/${project.artifactId}-${project.version}-java11-linux --add-modules javafx.controls,javafx.web,javafx.fxml --add-opens java.base/java.io=org.apache.tomcat.embed.core -jar ${project.artifactId}-${project.version}.jar
+cd ~/Downloads/${project.artifactId}-${project.version}-java11-*
+export LD_LIBRARY_PATH=.
+java --module-path . --add-modules javafx.controls,javafx.web,javafx.fxml --add-opens java.base/java.io=org.apache.tomcat.embed.core -jar ${project.artifactId}-${project.version}.jar
