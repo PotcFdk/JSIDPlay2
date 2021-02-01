6f685d6f2acb9271d2edb4088f2fffd6512aa19e
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-02-01 20:27:26 +0000

    credits fixed
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3124 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/builder/sidblaster/SIDBlasterEmu.java
+++ jsidplay2/src/main/java/builder/sidblaster/SIDBlasterEmu.java
@@ -203,12 +203,12 @@ public class SIDBlasterEmu extends SIDEmu {
 
 	public static final String credits() {
 		final StringBuffer credits = new StringBuffer();
-		credits.append("SIDBlaster Java version by Ken Händel <kschwiersch@yahoo.de> Copyright (©) 2007\n");
+		credits.append("SIDBlaster Java version by Ken Händel <kschwiersch@yahoo.de> Copyright (©) 2020\n");
 		credits.append("\tSupported by SIDBlaster-USB TicTac Edition (Andreas Schumm)\n");
 		credits.append("\thttp://crazy-midi.de\n");
+		credits.append("\tBased on SIDBlaster-USB by Davey (Das Phantom)\n");
+		credits.append("\tDLL created by Stein Pedersen\n");
 		credits.append("\tSIDBlaster test song by Hannes Malecki (Honey) of Welle: Erdball\n");
-		credits.append("\tBased on HardSID V1.0.1 Engine:\n");
-		credits.append("\tCopyright (©) 1999-2002 Simon White <sidplay2@yahoo.com>\n");
 		return credits.toString();
 	}
 
