16c80ea136769f7d79009e16fa5e4626b442244e
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-02-03 21:39:37 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3129 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/gamebase/GameBasePage.java
+++ jsidplay2/src/main/java/ui/gamebase/GameBasePage.java
@@ -12,7 +12,6 @@ import javafx.beans.property.DoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
@@ -71,31 +70,24 @@ public class GameBasePage extends C64VBox implements UIPart {
 			}
 		});
 		gamebaseTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-			Task<Void> task = new Task<Void>() {
-				@Override
-				public Void call() throws Exception {
-					if (newValue != null) {
-						if (newValue.getScreenshotFilename().isEmpty()) {
-							System.out.println("Screenshot is not available on GameBase64: " + newValue.getName());
-						} else {
-							try {
-								URL url = new URL(util.getConfig().getOnlineSection().getGb64ScreenshotUrl()
-										+ newValue.getScreenshotFilename().replace('\\', '/'));
-								if (screenshot == null) {
-									screenshot = (ImageView) gamebaseTable.getScene().lookup("#screenshot");
-								}
-								if (screenshot != null) {
-									Platform.runLater(() -> screenshot.setImage(new Image(url.toExternalForm())));
-								}
-							} catch (MalformedURLException e) {
-								System.err.println(e.getMessage());
-							}
+			if (newValue != null) {
+				if (newValue.getScreenshotFilename().isEmpty()) {
+					System.out.println("Screenshot is not available on GameBase64: " + newValue.getName());
+				} else {
+					try {
+						URL url = new URL(util.getConfig().getOnlineSection().getGb64ScreenshotUrl()
+								+ newValue.getScreenshotFilename().replace('\\', '/'));
+						if (screenshot == null) {
+							screenshot = (ImageView) gamebaseTable.getScene().lookup("#screenshot");
 						}
+						if (screenshot != null) {
+							Platform.runLater(() -> screenshot.setImage(new Image(url.toExternalForm())));
+						}
+					} catch (MalformedURLException e) {
+						System.err.println(e.getMessage());
 					}
-					return null;
 				}
-			};
-			new Thread(task).start();
+			}
 		});
 	}
 
