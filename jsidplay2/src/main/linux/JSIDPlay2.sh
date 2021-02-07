0aa6a49d4d45a34780b83087b25c4417e53b1b52
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-02-07 05:05:57 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3151 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/JSIDPlay2.sh
+++ jsidplay2/src/main/linux/JSIDPlay2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Downloads/${project.artifactId}-${project.version}
-export LD_LIBRARY_PATH=~/Downloads/${project.artifactId}-${project.version}
+export LD_LIBRARY_PATH=.
 java -jar ${project.artifactId}-${project.version}.jar
