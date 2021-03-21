7c49ef1f4f218ae229996458301aa4d50a5a43b6
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-03-21 22:11:12 +0000

    path fix
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3254 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/SIDBlasterTool.sh
+++ jsidplay2/src/main/linux/SIDBlasterTool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/Downloads/${project.artifactId}-${project.version}
+cd ~/Downloads/${project.artifactId}-${project.version}*
 java -jar ${project.artifactId}_sidblastertool-${project.version}.jar "$@"
