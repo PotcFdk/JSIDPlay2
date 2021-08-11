fbaf085a2198ea826ec19e678afdc2740ea0493e
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2021-08-11 20:18:03 +0000

    make JSIDPlay2 using Java11 launch again
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3528 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/JSIDPlay2 (Java11).sh
+++ jsidplay2/src/main/linux/JSIDPlay2 (Java11).sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=.
-java --module-path . --add-modules javafx.controls,javafx.web,javafx.fxml --add-opens java.base/java.io=org.apache.tomcat.embed.core -jar ./${project.artifactId}-${project.version}.jar "$@"
+java --module-path . --add-modules javafx.controls,javafx.web,javafx.fxml --add-opens java.base/java.io=org.apache.tomcat.embed.core --add-opens java.base/java.util.function=jcommander -jar ./${project.artifactId}-${project.version}.jar "$@"
