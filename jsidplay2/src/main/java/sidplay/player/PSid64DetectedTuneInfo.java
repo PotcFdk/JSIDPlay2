cecd567b5a15f44c8072557955f8a0da4da1c0a7
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-02-26 23:02:20 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3812 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/sidplay/player/PSid64DetectedTuneInfo.java
+++ jsidplay2/src/main/java/sidplay/player/PSid64DetectedTuneInfo.java
@@ -27,6 +27,18 @@ public final class PSid64DetectedTuneInfo {
 		return cpuClock;
 	}
 
+	public ChipModel getUserChipModel() {
+		return chipModels.get(0);
+	}
+
+	public ChipModel getStereoChipModel() {
+		return chipModels.get(1);
+	}
+
+	public int getStereoAddress() {
+		return stereoAddress;
+	}
+
 	public boolean hasDifferentCPUClock(CPUClock clock) {
 		return cpuClock != null && cpuClock != clock;
 	}
@@ -35,32 +47,12 @@ public final class PSid64DetectedTuneInfo {
 		return chipModels.size() > 0 && userSidModel != chipModels.get(0);
 	}
 
-	public ChipModel getUserChipModel() {
-		return chipModels.get(0);
-	}
-
 	public boolean hasDifferentStereoChipModel(ChipModel stereoSidModel) {
 		return chipModels.size() > 1 && stereoSidModel != chipModels.get(1);
 	}
 
-	public ChipModel getStereoChipModel() {
-		return chipModels.get(1);
-	}
-
-	public boolean isMonoTune() {
-		return chipModels.size() == 1;
-	}
-
-	public boolean isStereoTune() {
-		return chipModels.size() == 2;
-	}
-
 	public boolean hasDifferentStereoAddress(int dualSidBase) {
 		return stereoAddress != 0 && dualSidBase != stereoAddress;
-	}
-
-	public int getStereoAddress() {
-		return stereoAddress;
 	}
 
 }
